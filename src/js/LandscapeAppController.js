fe6a3c0d981fb7cfa8aae96fe060f0b5400909f1
Lukas Leitner <leitner@applics.at>
2015-12-21 13:50:57 +0100

    landscapeController
    -changes from Matthias

--- src/js/LandscapeAppController.js
+++ src/js/LandscapeAppController.js
@@ -23,46 +23,52 @@ var splaceLandscapeAppController = (function($) {
 			url: '/apps/' + app.name + '/' + app.name + '.html', 
 			async: false,
 			success: function(content) {
+				// load HTML code
 				var appContent = $(content).filter('#splaceApp');
 				splaceApp.append(appContent);
+
+				// inject scripts
+				$('#appscript').remove();
+				var buf = [];
+				buf.push('<script id="appscript" src="');
+				buf.push('/apps/'+app.name);
+				buf.push('/');
+				buf.push(app.name);
+				buf.push('.js');
+				buf.push('"></script>');
+				$('body').append(buf.join(''));
+				
+				// inject stylesheet
+				$('#appstyle').remove();
+				buf = [];
+				buf.push('<link id="appstyle" rel="stylesheet" href="');
+				buf.push('/apps/'+app.name);
+				buf.push('/');
+				buf.push(app.name);
+				buf.push('.css');
+				buf.push('">');
+				$('head').append(buf.join(''));
+
+				window[app.name + '_startup']();
+				loaded[app.name] = true;
 			},
 			error: function() {
 				console.log('There was an error loading your app called \'' + name + '\' please check the name again.');
 			}
 		});
-
-
-
-		$('#appscript').remove();
-
-		var buf = [];
-		buf.push('<script id="appscript" src="');
-		buf.push('/apps/'+app.name);
-		buf.push('/');
-		buf.push(app.name);
-		buf.push('.js');
-		buf.push('"></script>');
-
-		$('body').append(buf.join(''));
-
-		$('#appstyle').remove();
-
-		buf = [];
-		buf.push('<link id="appstyle" rel="stylesheet" href="');
-		buf.push('/apps/'+app.name);
-		buf.push('/');
-		buf.push(app.name);
-		buf.push('.css');
-		buf.push('">');
-
-		$('head').append(buf.join(''));
-
-		window[app.name + '_startup']();
-		loaded[app.name] = true;
 	}
 
 	function removeApp() {
-
+		try {
+			window[app.name + '_teardown']();
+		} catch(e) {
+			console.log('No teardown for ' + app.name);
+			console.log(e);
+		}
+		$('#appscript').remove();
+		$('#appstyle').remove();
+		$('#splaceApp').remove();
+		loaded[app.name] = false;
 	}
 
 	function setApp(name) {
